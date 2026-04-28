3ccd719e2145d56c768c37c143645e845e125f88
まえかわかいり <e245754@ie.u-ryukyu.ac.jp>
2026-04-28 11:31:48 +0900

    演習1.2

--- ex4/dataset1.py
+++ ex4/dataset1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pandas as pd
 
 def true_function(x):
     """
@@ -13,13 +13,27 @@ def true_function(x):
 x = np.linspace(-1, 1, 100)
 y = true_function(x)
 
+np.random.seed(0)
+xdot = np.random.uniform(-1, 1, 20)
+ydot = true_function(xdot)
+
+df = pd.DataFrame({
+    "観測点": xdot,
+    "真値": ydot
+})
+
+print(df)
+
 plt.plot(x, y, label="y = sin(pi * x * 0.8) * 10")
+plt.scatter(df["観測点"], df["真値"], color="red", label="observations")
+
 plt.legend() #凡例
 plt.xlabel("x")
 plt.ylabel("y")
 plt.grid()
-plt.savefig("ex1.1.png") #ex1.1.pngとして保存
+plt.savefig("ex1.2.png") #ex1.1.pngとして保存
 plt.show()
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
