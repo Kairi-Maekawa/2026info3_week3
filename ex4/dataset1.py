7b234613c7010496a16ac3f03f3fcd967063824e
まえかわかいり <e245754@ie.u-ryukyu.ac.jp>
2026-04-28 11:44:11 +0900

    演習1.3

--- ex4/dataset1.py
+++ ex4/dataset1.py
@@ -22,16 +22,20 @@ df = pd.DataFrame({
     "真値": ydot
 })
 
+noise = np.random.normal(loc=0.0, scale=np.sqrt(2.0), size=20)
+noise = noise / 2
+df["観測値"] = df["真値"] + noise
 print(df)
 
 plt.plot(x, y, label="y = sin(pi * x * 0.8) * 10")
 plt.scatter(df["観測点"], df["真値"], color="red", label="observations")
+plt.scatter(df["観測点"], df["観測値"], color="blue", label="observations_noise")
 
 plt.legend() #凡例
 plt.xlabel("x")
 plt.ylabel("y")
 plt.grid()
-plt.savefig("ex1.2.png") #ex1.1.pngとして保存
+plt.savefig("ex1.3.png") #ex1.1.pngとして保存
 plt.show()
 
 if __name__ == "__main__":
